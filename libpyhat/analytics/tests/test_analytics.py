f3e0affd7b8c08b4c7fd26d5bfa2b53c15f1d09c
Adam Paquette <acpaquette@usgs.gov>
2018-10-30 13:41:08 -0700

    Last update to analytics testing

--- libpyhat/analytics/tests/test_analytics.py
+++ libpyhat/analytics/tests/test_analytics.py
@@ -1,21 +1,23 @@
 import pytest
 
 import numpy as np
+from numpy.testing import assert_array_equal
 
 from libpyhat.analytics import analytics
 
 @pytest.fixture
 def setUp():
-    np.random.seed(seed=42)
+    np.random.seed(seed=12345)
     return np.random.random(25)
 
 def test_band_minima(setUp):
     minidx, minvalue = analytics.band_minima(setUp)
-    assert minidx == 10
-    assert minvalue == pytest.approx(0.02058449)
+    print(setUp)
+    assert minidx == 12
+    assert minvalue == pytest.approx(0.008388297)
 
 @pytest.mark.parametrize("lower_bound, upper_bound, expected_idx, expected_val", [
-                                            (0, 7, 6, 0.05808361),
+                                            (0, 7, 2, 0.18391881),
                                             pytest.param(6, 1, 0, 0, marks=pytest.mark.xfail)]
 )
 def test_band_minima_bounds(lower_bound, upper_bound, expected_idx, expected_val, setUp):
@@ -23,11 +25,15 @@ def test_band_minima_bounds(lower_bound, upper_bound, expected_idx, expected_val
     assert minidx == expected_idx
     assert minvalue == pytest.approx(expected_val)
 
-def test_band_center(setUp):
-    center, center_fit = analytics.band_center(setUp)
-    assert center_fit[0] == max(center_fit)
-    assert center_fit[-1] == min(center_fit)
-    assert center[0] == 10
+@pytest.mark.parametrize("spectrum, expected_zero_idx, expected_neg_one_idx, expected_center", [
+                                            (setUp(), 0.56293697, 0.42828491, [12]),
+                                            (np.ones(24), 1, 1, np.array(range(24)))]
+)
+def test_band_center(spectrum, expected_zero_idx, expected_neg_one_idx, expected_center):
+    center, center_fit = analytics.band_center(spectrum)
+    assert center_fit[0] == pytest.approx(expected_zero_idx)
+    assert center_fit[-1] == pytest.approx(expected_neg_one_idx)
+    assert_array_equal(center[0], expected_center)
 
 def test_band_area():
     x = np.arange(-2, 2, 0.1)
@@ -37,7 +43,7 @@ def test_band_area():
     assert area == [370.5]
 
 @pytest.mark.parametrize("spectrum, expected_val", [
-                                            (setUp(), 0.32432432),
+                                            (setUp(), 0),
                                             (np.ones(24), 0)]
 )
 def test_band_asymmetry(spectrum, expected_val):
